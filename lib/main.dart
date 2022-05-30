29ef1c73018740b4cb1c28ca196c664e02a3d10b
shivam <shivam@example.com>
2022-05-31 01:15:18 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 import "package:flutter/material.dart";
-//import 'package:flutter_myapp/pages/login_page.dart';
-import 'pages/home_page.dart';
+import 'package:flutter_myapp/pages/login_page.dart';
+import 'package:flutter_myapp/pages/home_page.dart';
 void main()
 {
 runApp(yapp());
@@ -17,11 +17,10 @@ class yapp extends StatelessWidget {
          darkTheme: ThemeData(
          brightness: Brightness.dark,
          ),
-         initialRoute:"/home",
+         //initialRoute:"/",
         routes : {
-         // "/":(context)=> LoginPage(),
-          "/home":(context) => HomePage(),
-          //"/login":(context) => LoginPage()
+          "/":(context)=> LoginPage(),
+          "/home":(context)=> HomePage(),
        },
     );
 }
--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -1,6 +1,5 @@
-
-
 import "package:flutter/material.dart";
+
 class LoginPage extends StatelessWidget {
 
 
