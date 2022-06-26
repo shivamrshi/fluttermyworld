adcb0de92b7319be0d981e201780bff937ac2c43
shivam <shivam@example.com>
2022-06-27 02:18:08 +0530

    commmm

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,3 +1,6 @@
+
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_myapp/pages/shop.dart';
@@ -12,35 +15,49 @@ class MyDrawer extends StatelessWidget {
     "https://cdn4.iconfinder.com/data/icons/education-and-learning-25/256/Smart_learning-512.png";
     return Drawer(
      
-      child: ListView(
-         padding: EdgeInsets.zero,
-          
-       
-          
-           
+      child: ListView( 
+        padding: EdgeInsets.zero,
+        
          children:[
-            UserAccountsDrawerHeader(
-                  
-               margin: EdgeInsets.zero,
-             
-               
-               accountName:Text("John Tim"),
-               decoration:BoxDecoration(
-                 color: Color.fromARGB(255, 190, 255, 130),
-                 borderRadius: BorderRadius.circular(40),
-                
-               
-               ),
-               
-               accountEmail: Text("r.kstarpoint@gmail.com"),
-             currentAccountPicture:CircleAvatar(
-               backgroundImage: NetworkImage(imageUrl),
-               ) ,
+           SingleChildScrollView(
+             child: Container(
+              margin: EdgeInsets.all(0),
+              height:300,
+              width:double.maxFinite,
+              decoration: BoxDecoration(color:Color.fromARGB(255, 190, 255, 130),
+              borderRadius: BorderRadius.circular(30)),
+              child:Row(children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:26,),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/users.png"),radius: 80,
+                  ),
+                ),
+                SizedBox(width:10),
+                Padding(
+                  padding: const EdgeInsets.only(top:150),
+                  child: Column(
+                    children: [
+                      Text("John Tim", style:TextStyle(fontSize:30,fontWeight: FontWeight.bold,color:Color.fromARGB(244, 50, 48, 48),)
+                      ),
+                       Text("Edit Profile",textAlign: TextAlign.start, style:TextStyle(fontSize:16,fontWeight: FontWeight.bold,color:Color.fromARGB(244, 50, 48, 48),)
+                      ),
+                    ],
+                  ),
+                ),
+                 SizedBox(width:10),
+                 Padding(
+                   padding: const EdgeInsets.only(top:50),
+                   child: Column(
+                     children: [
+                       IconButton(onPressed: (){Navigator.pushNamed(context, MyRoutes.mainpageRoute);},  icon: Icon(Icons.arrow_back_ios,color: Color.fromARGB(255, 35, 33, 33),)),
+                     ],
+                   ),
+                 )
 
-               
-          
+              ],)
              ),
-             //)
+           ),
       
           
          
